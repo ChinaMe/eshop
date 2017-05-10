b186cd1cc22761f23a2b2464c329d3193632f00c
ChinaMe <chaoy.china@gmail.com>
2017-05-10 18:40:39 +0800

    去除header的菜单复杂效果

--- eshop/src/main/webapp/static/js/menu.js
+++ eshop/src/main/webapp/static/js/menu.js
@@ -12,7 +12,7 @@ $(function(){
 	html += "<div id=\"head\">";
 	html += "<div id=\"nav\">";
 	html += "<ul>";
-	html += "<li class=\"nav_lishw\"><div class=\"v\"><a href=\"javascript:;return;\", page=\"index\" class=\"\"><span class=\"l\"></span>首　页</a></div></li>";
+	html += "<li class=\"\"><div class=\"v\"><a href=\"javascript:;return;\", page=\"index\" class=\"\"><span class=\"l\"></span>首　页</a></div></li>";
 	html += "<li class=\"\"><div class=\"v\"><a href=\"javascript:;return;\", page=\"aboutus\"><span class=\"l\"></span>关于我们</a></div></li>";
 	html += "<li class=\"\"><div class=\"v\"><a href=\"javascript:;return;\", page=\"products\"><span class=\"l\"></span>产品展示</a></div></li>";
 	html += "<li class=\"\"><div class=\"v\"><a href=\"javascript:;return;\", page=\"news\"><span class=\"l\"></span>公司新闻</a></div></li>";
@@ -26,11 +26,9 @@ $(function(){
 	
 	$("#nav ul li").each(function(){
 		$(this).click(function(){
-			$("#nav ul li div a").removeClass("sele");
-			$(this).find("div").find("a").addClass("sele");
 			var page = $(this).find("div").find("a").attr("page");
 			if(page.indexOf("index") >= 0) {
-				window.open("index.html", "_self");
+				window.open("index.html?page="+page, "_self");
 			} else {
 				window.open("base.html?page="+page, "_self", 'height=100%, width=500, top=0, left=0, toolbar=no, menubar=no, scrollbars=no, resizable=no, location=no, status=no');
 			}
@@ -38,53 +36,71 @@ $(function(){
 	});
 	
 	var page = getQueryString("page");
-	if(page == "index") {
-		window.open("index.html", "_self");
+	if(page === null || page == "index") {
+		setMenuCss();
 	} else if(page == "aboutus") {
+		setMenuCss();
 		$("#right").attr("src", "aboutus/aboutus.html");
 	} else if(page == "products") {
+		setMenuCss();
 		$("#right").attr("src", "product/products.html");
 	} else if(page == "news") {
+		setMenuCss();
 		$("#right").attr("src", "news/news.html");
 	} else if(page == "inquiry") {
+		setMenuCss();
 		$("#right").attr("src", "inquiry/inquiry.html");
 	} else if(page == "order") {
+		setMenuCss();
 		$("#right").attr("src", "order/order.html");
 	} else if(page == "contact") {
+		setMenuCss();
 		$("#right").attr("src", "contact/contact.html");
 	} 
 	
+	function setMenuCss() {
+		$("#nav li a").each(function() {
+			var temp = $(this).attr("page");
+			if(page === null || page == "index") {
+				$("#nav_hover .v a").removeClass("sele");
+				$("#nav li a").eq(0).addClass("sele");
+			} 
+			if(temp == page) {
+				$("#nav_hover .v a").removeClass("sele");
+				$(this).addClass("sele");
+			}
+		});
+	}
+	
+	/*
 	switch (true) {		
 		default :
-			$("#nav li").attr("class","");
-			$("#nav li").eq(0).attr("class","nav_lishw");
-			$(".nav_lishw .v a").attr("class","sele");
-			$(".nav_lishw .kind_menu").show();
+			if(page == "" || page == "undefined") {
+				$("#nav li").attr("class","");
+				$("#nav li").eq(0).attr("class","nav_lishw");
+				$(".nav_lishw .v a").attr("class","sele");
+				$(".nav_lishw .kind_menu").show();
+			}
 	} 
 	
 	$("#nav li").hover(
 		function(){
 			clearTimeout(setTimeout("0")-1);
-			$("#nav .kind_menu").hide(); 
 			$("#nav li .v .sele").attr("class","shutAhover");
 			$(this).attr("id","nav_hover")
-			$("#nav_hover .v a").attr("class","sele");
-			$("#nav_hover .kind_menu").show(); 
+			$("#nav_hover .v a").attr("class", "sele");
 		},
 		function(){
-			
 			if($(this).attr("class") != "nav_lishw"){
 				$("#nav_hover .v .sele").attr("class","");
-				$("#nav_hover .kind_menu").hide(); 
 			}
 			$(this).attr("id","")
 			$("#nav li .v .shutAhover").attr("class","sele");
 			setTimeout(function(){
-				$(".nav_lishw .kind_menu").show();
 				$(".nav_lishw .v a").attr("class","sele");
 			},50); 
 		}
-	);
+	);*/
 	
 	function getQueryString(name) { 
 		var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i"); 
